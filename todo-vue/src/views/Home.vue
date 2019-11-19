679286152a154a2a1baab27ded5c0ad81bb25274
mooyeon-choi <dus1208@ajou.ac.kr>
2019-11-19 10:07:10 +0900

    Vue | TodoForm component | Todo 등록

--- todo-vue/src/views/Home.vue
+++ todo-vue/src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <!-- 이벤트 리스너 등록 -->
+    <TodoForm @todoCreate-event="todoCreate"/> <!-- PascalCase, uppercamelcase -->
+    <!-- kebab-case -->
     <TodoList :todos="todos"/>
   </div>
 </template>
@@ -9,11 +12,13 @@
 // @ is an alias to /src
 import axios from 'axios'
 import TodoList from '@/components/TodoList.vue'
+import TodoForm from '@/components/TodoForm.vue'
 
 export default {
   name: 'home',
   components: {
-    TodoList
+    TodoList,
+    TodoForm
   },
   data() {
     // 컴포넌트에서는 반드시 data를 함수로 작성하고, object를 리턴한다
@@ -21,16 +26,42 @@ export default {
       todos: [],
     }
   },
+  methods: {
+    todoCreate(title) {
+      console.log('==부모컴포넌트==')
+      console.log(title)
+      // axios 요청 POST /todos/
+      const data = {
+        title: title,
+        user: 1
+      }
+      // request.POST인 경우는 반드시 FormData!
+      // const formData = new FormData()
+      // formData.append('title', title)
+      // formData.append('user', 1)
+      axios.post('http://127.0.0.1:8000/api/v1/todos/', data)
+        .then(response => {
+          console.log(response)
+          this.todos.push(response.data)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+    getTodos() {
+      // axios 요청
+      axios.get('http://127.0.0.1:8000/api/v1/todos/')
+        .then(response => {
+          console.log(response) // 만약, 오류가 발생한다면 ESLint 설정을 package.json에 추가
+          this.todos = response.data
+        })
+        .catch(error => {
+          console.log(error)
+        })      
+    }
+  },
   mounted() {
-    // axios 요청
-    axios.get('http://127.0.0.1:8000/api/v1/todos/')
-      .then(response => {
-        console.log(response) // 만약, 오류가 발생한다면 ESLint 설정을 package.json에 추가
-        this.todos = response.data
-      })
-      .catch(error => {
-        console.log(error)
-      })
+    this.getTodos()
   }
 }
 </script>
